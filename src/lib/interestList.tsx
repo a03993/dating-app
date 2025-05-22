bce4a4de2264c106d89516342f8ed708a1dd9627
Tina <a03993@yahoo.com.tw>
2025-05-23 00:53:55 +0800

    refactor: move interestList to a separate file and update InterestSelectGroup component

---
+++ src/lib/interestList.tsx
@@ -0,0 +1,87 @@
+import CameraIcon from "../assets/icons/Camera.svg?react"
+import GameHandleIcon from "../assets/icons/GameHandle.svg?react"
+import GobletFullIcon from "../assets/icons/GobletFull.svg?react"
+import MusicIcon from "../assets/icons/Music.svg?react"
+import NoodlesIcon from "../assets/icons/Noodles.svg?react"
+import OutdoorIcon from "../assets/icons/Outdoor.svg?react"
+import ParachuteIcon from "../assets/icons/Parachute.svg?react"
+import PlatteIcon from "../assets/icons/Platte.svg?react"
+import RippleIcon from "../assets/icons/Ripple.svg?react"
+import ShopBagIcon from "../assets/icons/ShopBag.svg?react"
+import SportIcon from "../assets/icons/Sport.svg?react"
+import TennisIcon from "../assets/icons/Tennis.svg?react"
+import VienchartsIcon from "../assets/icons/Viencharts.svg?react"
+import VoiceIcon from "../assets/icons/Voice.svg?react"
+
+export const interestList = [
+  {
+    id: "photography",
+    label: "Photography",
+    icon: CameraIcon,
+  },
+  {
+    id: "shopping",
+    label: "Shopping",
+    icon: ShopBagIcon,
+  },
+  {
+    id: "karaoke",
+    label: "Karaoke",
+    icon: VoiceIcon,
+  },
+  {
+    id: "yoga",
+    label: "Yoga",
+    icon: VienchartsIcon,
+  },
+  {
+    id: "cooking",
+    label: "Cooking",
+    icon: NoodlesIcon,
+  },
+  {
+    id: "tennis",
+    label: "Tennis",
+    icon: TennisIcon,
+  },
+  {
+    id: "running",
+    label: "Running",
+    icon: SportIcon,
+  },
+  {
+    id: "swimming",
+    label: "Swimming",
+    icon: RippleIcon,
+  },
+  {
+    id: "art",
+    label: "Art",
+    icon: PlatteIcon,
+  },
+  {
+    id: "traveling",
+    label: "Traveling",
+    icon: OutdoorIcon,
+  },
+  {
+    id: "extreme",
+    label: "Extreme",
+    icon: ParachuteIcon,
+  },
+  {
+    id: "music",
+    label: "Music",
+    icon: MusicIcon,
+  },
+  {
+    id: "drinking",
+    label: "Drinking",
+    icon: GobletFullIcon,
+  },
+  {
+    id: "videoGames",
+    label: "Video Games",
+    icon: GameHandleIcon,
+  },
+]
